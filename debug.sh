f3669478064d68af531fd0e4ab79addf3ad89b59
BotPhil01 <acsrobles39+github@proton.me>
2025-12-18 18:28:05 +0000

    Feat: premake and test code

---
+++ debug.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+premake5 --cc=gcc --verbose gmake
---
+++ premake5.lua
@@ -0,0 +1,18 @@
+workspace "filefaff"
+     configurations { "debug", "release" }
+
+project "filefaff"
+    kind "ConsoleApp"
+    language "C"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "build/%{cfg.buildcfg}"
+
+    files { "include/**.h", "src/**.c" }
+
+    filter "configurations:debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:release"
+        defines { "NDEBUG" }
+        optimize "On"
